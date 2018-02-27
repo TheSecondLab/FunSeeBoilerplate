eaf8651120b179859c848568c39da72d19df6ea8
MeatPieYan <robinyzg@hotmail.com>
2018-02-27 11:55:32 +0800

    redux test

--- shared/module1/Container.js
+++ shared/module1/Container.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import connect from '../../../FunSee/lib/tool/connector';
 
 
-const Module1 = () => (
+const Module1 = (props) => (
   <div>
     hellow 1112223
+    {props.test}
   </div>
 );
 
-export default Module1;
+export default connect(state => {
+  return {
+    test: state.text
+  };
+})(Module1);
---
+++ test/action.js
@@ -0,0 +1,3 @@
+export const ADD_TODO = 'ADD_TODO';
+
+export const TODO_ACTION = { type: ADD_TODO, payload: 'test' };
